796f094a3e158e4baae3aa84b0299c29d36adea1
dhirajj14 <djain14@hawk.iit.edu>
2020-07-04 19:30:57 -0500

    add contact details

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,48 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.5),
               ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.black.withOpacity(0.8),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "+1 312 536 9875",
+                      style: TextStyle(
+                          fontFamily: 'SourceSan', color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.black.withOpacity(0.8),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "dhirajj75@gmail.com",
+                      style: TextStyle(
+                          fontFamily: 'SourceSan', color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
