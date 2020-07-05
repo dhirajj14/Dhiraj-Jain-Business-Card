d63d1b86d3e3ef11e3414f8864810a19076c0e63
dhirajj14 <djain14@hawk.iit.edu>
2020-07-04 19:16:26 -0500

    add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,15 +9,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: new AppBar(
-          title: Text("Dhiraj Jain"),
-          backgroundColor: Colors.amberAccent,
-        ),
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              CircleAvatar(),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("assets/images/dhiraj.jpg"),
+              ),
+              Text(
+                "Dhiraj Jain",
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontFamily: "Pacifico"),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                    color: Colors.white.withOpacity(0.5),
+                    fontFamily: "SourceSans",
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
             ],
           ),
         ),
